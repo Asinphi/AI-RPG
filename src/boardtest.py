700dfa6a5285dd05dd37fd504554942c571098ee
michaelkalai <michael.kalai124@gmail.com>
2023-12-04 16:42:28 -0500

    testing for 2d array

--- src/boardtest.py
+++ src/boardtest.py
@@ -1,11 +1,17 @@
 from src.board import Board
 from boardmatrix import BoardMatrix
+from twodarray import TwoDArray
 
-board = Board()
-adjlist = board.get_adj_list()
-print(adjlist)
+# board = Board()
+# adjlist = board.get_adj_list()
+# print(adjlist)
 
 # boardmat = BoardMatrix()
 # adjmat = boardmat.get_matrix()
 # for row in adjmat:
+#     print(row)
+
+# boardtwod = TwoDArray(3, 3)
+# arr = boardtwod.get_array()
+# for row in arr:
 #     print(row)
